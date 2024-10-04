35143b6744d4a97ece6e8a6ada7f7e1a4f6a8f41
serghine <sergh.boudj@gmail.com>
2024-10-04 15:05:39 +0200

    first commit initializing application

---
+++ src/test/java/com/villageEmploie/villageEmploisApi/VillageEmploisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.villageEmploie.villageEmploisApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VillageEmploisApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
